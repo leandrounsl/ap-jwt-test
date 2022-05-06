127eccaa726dcead0e138e874a477c1ef3894b28
Leandro Giménez <ex_neccolombia_bio04@sunat.gob.pe>
2022-05-06 17:29:46 -0300

    Se cambia a GetMapping el endpoint /hello

--- src/main/java/com/argentina/programa/jwt/test/controller/HelloWorldController.java
+++ src/main/java/com/argentina/programa/jwt/test/controller/HelloWorldController.java
@@ -1,14 +1,14 @@
 package com.argentina.programa.jwt.test.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin()
 public class HelloWorldController {
 
-	@RequestMapping({ "/hello" })
+	@GetMapping({ "/hello" })
 	public String hello() {
 		// Este endpoint respondera solo si el usuario que realiza el request esta
 		// autenticado.
